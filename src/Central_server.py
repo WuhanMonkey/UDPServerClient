b9f368f420678b2aed4eaa93a2beb406dc3df311
Chengpeng Hu <hcp199242@gmail.com>
2015-03-09 15:47:39 -0500

    test

--- src/Central_server.py
+++ src/Central_server.py
@@ -129,6 +129,7 @@ class Central_server(object):
                     continue
                 if message:
                     for s in self.server_list:
+                        print 'hello sucker'
                         self.s_send.sendto(message, (self.h, int(s)))
                 
 
